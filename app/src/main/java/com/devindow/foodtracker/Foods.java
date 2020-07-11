e724d9393e273d2fbf7a456f5cdb8eb7942941b4
Devin Dow <DevinDow@gmail.com>
2020-07-11 10:31:53 -0700

    experimenting with Gson for persistence

--- app/src/main/java/com/devindow/foodtracker/Foods.java
+++ app/src/main/java/com/devindow/foodtracker/Foods.java
@@ -5,23 +5,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import com.google.gson.Gson;
 
 public class Foods {
 
     public static final List<Food> ITEMS = new ArrayList<>();
 
-    public static final Map<String, Food> ITEM_MAP = new HashMap<>();
+    //public static final Map<String, Food> ITEM_MAP = new HashMap<>();
 
     static {
         // Add some sample items.
         for (int i = 1; i <= 25; i++) {
             addItem(createDummyItem(i));
         }
+
+        Gson gson = new Gson();
+        String json = gson.toJson(Foods.ITEMS);
+
+        Object o = gson.fromJson(json, ArrayList.class);
+
+        String json2 = gson.toJson(o);
+
+        if (json.compareTo(json2) != 0)
+        {
+        }
     }
 
     private static void addItem(Food item) {
         ITEMS.add(item);
-        ITEM_MAP.put(item.id, item);
+        //ITEM_MAP.put(item.id, item);
     }
 
     private static Food createDummyItem(int position) {
