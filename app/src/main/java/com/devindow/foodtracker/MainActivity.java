adcd77d8b2acdec1400f439aa1d0b5c518a3a0b0
Devin Dow <DevinDow@gmail.com>
2020-07-11 14:24:46 -0700

    Floating Actrin Button for Adding Food

--- app/src/main/java/com/devindow/foodtracker/MainActivity.java
+++ app/src/main/java/com/devindow/foodtracker/MainActivity.java
@@ -3,6 +3,10 @@ package com.devindow.foodtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FloatingActionButton
+        FloatingActionButton fab = findViewById(R.id.fabAdd);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
